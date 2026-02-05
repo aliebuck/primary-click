fb3b8d9d63080e987d4c55eedc5ec76f0e72f6ef
Alie Buck <830470+aliebuck@users.noreply.github.com>
2026-02-05 21:12:23 +1300

    fix: preserve this binding in onPrimaryClick

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ export const isPrimaryClick = (event) =>
  * @param {Function} func
  * @return {Function}
  */
-export const onPrimaryClick =
-  (func) =>
-  (event, ...args) =>
-    isPrimaryClick(event) ? func(event, ...args) : undefined;
+export const onPrimaryClick = (func) =>
+  function (event, ...args) {
+    if (isPrimaryClick(event)) return func.call(this, event, ...args);
+  };
